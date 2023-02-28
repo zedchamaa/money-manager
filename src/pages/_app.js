e7c0a528adbd9fdb01ca30f18dfac44dd9401c76
Zed Chamaa <sales@nineprovince.com>
2023-02-28 09:31:18 +0100

    Wrap app with the firebase auth context provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { AuthContextProvider } from '@/context/AuthContext'
+
+// styles
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
 }
