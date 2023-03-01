a306b6b3cfa3aaaa7407374be53d609061da5500
Zed Chamaa <sales@nineprovince.com>
2023-03-01 14:54:08 +0100

    Set conditional page redirects for user

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import { AuthContextProvider } from '@/context/AuthContext'
+import { useRouter } from 'next/router'
+import { useAuthContext } from '@/hooks/useAuthContext'
+import { useEffect } from 'react'
 
 // styles
 import '@/styles/globals.css'
@@ -6,7 +9,39 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <AuthContextProvider>
-      <Component {...pageProps} />
+      <AppWrapper
+        Component={Component}
+        pageProps={pageProps}
+      />
     </AuthContextProvider>
   )
+}
+
+function AppWrapper({ Component, pageProps }) {
+  const router = useRouter()
+  const currentPath = router.pathname
+  const { authIsReady, user } = useAuthContext()
+
+  useEffect(() => {
+    if (authIsReady) {
+      if (currentPath === '/login' && user) {
+        router.push('/')
+      }
+      if (currentPath === '/signup' && user) {
+        router.push('/')
+      }
+      if (
+        !user &&
+        currentPath !== '/login' &&
+        !user &&
+        currentPath !== '/signup' &&
+        !user &&
+        currentPath !== '/forgot-password'
+      ) {
+        router.push('/login')
+      }
+    }
+  }, [authIsReady, currentPath, user, router])
+
+  return <Component {...pageProps} />
 }
