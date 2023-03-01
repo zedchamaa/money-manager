291d6e5b176646ca4d3cd5f5b8c2edc53f167331
Zed Chamaa <sales@nineprovince.com>
2023-03-01 16:58:32 +0100

    Hide main pages content from users not logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
+import { useAuthContext } from '@/hooks/useAuthContext'
+
 export default function HomePage() {
+  const { user } = useAuthContext()
+
+  // hide the page content from non-logged in users
+  // always run this if statement first
+  if (!user) {
+    return
+  }
+
   return <div>Home Page</div>
 }
--- src/pages/transactions/year/[year].js
+++ src/pages/transactions/year/[year].js
@@ -1,8 +1,16 @@
 import { useRouter } from 'next/router'
+import { useAuthContext } from '@/hooks/useAuthContext'
 
 export default function TransactionsYear() {
   const router = useRouter()
   const { year } = router.query
+  const { user } = useAuthContext()
+
+  // hide the page content from non-logged in users
+  // always run this if statement first
+  if (!user) {
+    return
+  }
 
   return (
     <div>
