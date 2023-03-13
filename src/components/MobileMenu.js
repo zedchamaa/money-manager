95f4bf9144238c41927ca4528b131d8fd350caf8
Zed Chamaa <sales@nineprovince.com>
2023-03-13 10:57:01 +0100

    Conditionally color the menu item when selected

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,6 +1,6 @@
 // styles
 import styles from './MobileMenu.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -8,11 +8,39 @@ import { useRouter } from 'next/router'
 import Logo from './Logo'
 import HamburgerMenuIcon from './icons/HamburgerMenuIcon'
 import CloseIcon from './icons/CloseIcon'
+import GraphIcon from './icons/GraphIcon'
+import ConnectionIcon from './icons/ConnectionIcon'
 
 export default function MobileMenu() {
   const [menuIsClosed, setMenuIsClosed] = useState(true)
   const router = useRouter()
+  const currentPath = router.pathname
   const { year } = router.query
+  const [summarySelected, setSummarySelected] = useState(false)
+  const [transactionsSelected, setTransactionsSelected] = useState(false)
+
+  let defaultColor = '#667085'
+  let selectedColor = '#7F56D9'
+  let summaryColor
+  let transactionsColor
+
+  useEffect(() => {
+    if (currentPath.includes('summary')) {
+      setSummarySelected(true)
+      setTransactionsSelected(false)
+    } else {
+      setSummarySelected(false)
+      setTransactionsSelected(true)
+    }
+  }, [currentPath])
+
+  if (currentPath.includes('summary')) {
+    summaryColor = selectedColor
+    transactionsColor = defaultColor
+  } else {
+    transactionsColor = selectedColor
+    summaryColor = defaultColor
+  }
 
   const handleOpenMenu = () => {
     setMenuIsClosed(false)
@@ -32,12 +60,20 @@ export default function MobileMenu() {
       {!menuIsClosed && (
         <div className={styles.openedMenu}>
           <nav>
-            <div className={styles.navItem}>
-              <p>Icon</p>
+            <div
+              className={
+                summarySelected ? styles.navItemSelected : styles.navItem
+              }
+            >
+              <GraphIcon color={summaryColor} />
               <Link href='/transactions/summary/2024'>Summary</Link>
             </div>
-            <div className={styles.navItem}>
-              <p>Icon</p>
+            <div
+              className={
+                transactionsSelected ? styles.navItemSelected : styles.navItem
+              }
+            >
+              <ConnectionIcon color={transactionsColor} />
               <p>Transactions</p>
             </div>
           </nav>
