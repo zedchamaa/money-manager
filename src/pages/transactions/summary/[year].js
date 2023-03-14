b7479b23a4d7f873ef153abefb50ba4f5d7b6fa8
Zed Chamaa <sales@nineprovince.com>
2023-03-14 11:57:10 +0100

    Add the modal component to the summary page

--- src/pages/transactions/summary/[year].js
+++ src/pages/transactions/summary/[year].js
@@ -1,12 +1,15 @@
 import { useRouter } from 'next/router'
 import { useAuthContext } from '@/hooks/useAuthContext'
+import { useState } from 'react'
 
 // components
 import MobileMenu from '@/components/MobileMenu'
 import DesktopMenu from '@/components/DesktopMenu'
 import IntroTopMobile from '@/components/IntroTopMobile'
+import Modal from '@/components/Modal'
 
 export default function TransactionsSummary() {
+  const [showModal, setShowModal] = useState(false)
   const router = useRouter()
   const { year } = router.query
   const { user } = useAuthContext()
@@ -17,10 +20,32 @@ export default function TransactionsSummary() {
     return
   }
 
+  const handleShowModal = () => {
+    setShowModal(true)
+  }
+
+  const handleCancel = () => {
+    setShowModal(false)
+  }
+
+  const handleConfirm = () => {
+    console.log('Confirm button clicked')
+  }
+
   return (
     <div>
+      {showModal && (
+        <Modal
+          title='Add Transaction'
+          handleCancel={handleCancel}
+          handleConfirm={handleConfirm}
+        />
+      )}
       <MobileMenu />
-      <IntroTopMobile title='Summary' />
+      <IntroTopMobile
+        title='Summary'
+        onClick={handleShowModal}
+      />
       <DesktopMenu />
       {/* Your transaction summary can go here */}
     </div>
