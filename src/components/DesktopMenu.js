fcffa7c5c425426a05c50830c1101b63641c5727
Zed Chamaa <sales@nineprovince.com>
2023-03-14 10:18:30 +0100

    Create the desktop menu component

---
+++ src/components/DesktopMenu.js
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+// styles
+import styles from './DesktopMenu.module.css'
+
+// components
+import Logo from './Logo'
+import GraphIcon from './icons/GraphIcon'
+import ConnectionIcon from './icons/ConnectionIcon'
+import UserInfoDesktop from './UserInfoDesktop'
+import CallToActionButton from './CallToActionButton'
+
+export default function DesktopMenu() {
+  const [summarySelected, setSummarySelected] = useState(false)
+  const [transactionsSelected, setTransactionsSelected] = useState(false)
+  const router = useRouter()
+  const currentPath = router.pathname
+  const { year } = router.query
+
+  let defaultColor = '#667085'
+  let selectedColor = '#7F56D9'
+  let summaryColor
+  let transactionsColor
+
+  useEffect(() => {
+    if (currentPath.includes('summary')) {
+      setSummarySelected(true)
+      setTransactionsSelected(false)
+    } else {
+      setSummarySelected(false)
+      setTransactionsSelected(true)
+    }
+  }, [currentPath])
+
+  if (currentPath.includes('summary')) {
+    summaryColor = selectedColor
+    transactionsColor = defaultColor
+  } else {
+    transactionsColor = selectedColor
+    summaryColor = defaultColor
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Logo />
+        <nav>
+          <div
+            className={
+              summarySelected ? styles.navItemSelected : styles.navItem
+            }
+          >
+            <GraphIcon color={summaryColor} />
+            <Link href={`/transactions/summary/${year}`}>Summary</Link>
+          </div>
+          <div
+            className={
+              transactionsSelected ? styles.navItemSelected : styles.navItem
+            }
+          >
+            <ConnectionIcon color={transactionsColor} />
+            <Link href={`/transactions/year/${year}`}>Transactions</Link>
+          </div>
+        </nav>
+        <div className={styles.rightContainer}>
+          <CallToActionButton title='Add Transaction' />
+          <UserInfoDesktop />
+        </div>
+      </div>
+    </>
+  )
+}
