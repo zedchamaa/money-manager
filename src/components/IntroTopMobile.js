cb8bb985d3e310c556dd60000fa4575d6c1a8a35
Zed Chamaa <sales@nineprovince.com>
2023-03-14 11:58:00 +0100

    Add on click prop to intro top mobile component

--- src/components/IntroTopMobile.js
+++ src/components/IntroTopMobile.js
@@ -4,11 +4,14 @@ import styles from './IntroTopMobile.module.css'
 // components
 import CallToActionButton from './CallToActionButton'
 
-export default function IntroTopMobile({ title }) {
+export default function IntroTopMobile({ title, onClick }) {
   return (
     <div className={styles.container}>
       {title}
-      <CallToActionButton title='Add Transaction' />
+      <CallToActionButton
+        title='Add Transaction'
+        onClick={onClick}
+      />
     </div>
   )
 }
