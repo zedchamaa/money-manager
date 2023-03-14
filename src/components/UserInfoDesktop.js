c060355fd38bab5880d22ba53a80ab3055a68dbe
Zed Chamaa <sales@nineprovince.com>
2023-03-14 09:29:15 +0100

    Create and style the user info desktop component

---
+++ src/components/UserInfoDesktop.js
@@ -0,0 +1,28 @@
+import { useLogout } from '@/hooks/useLogout'
+import { useAuthContext } from '@/hooks/useAuthContext'
+
+// styles
+import styles from './UserInfoDesktop.module.css'
+
+// components
+import SignoutIcon from './icons/SignoutIcon'
+import ProfileIcon from './icons/ProfileIcon'
+
+export default function UserInfoDesktop() {
+  const { user } = useAuthContext()
+  const { logout } = useLogout()
+
+  const handleLogout = () => {
+    logout()
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.userInfo}>
+        <ProfileIcon />
+        {user.displayName}
+        <SignoutIcon onClick={handleLogout} />
+      </div>
+    </div>
+  )
+}
