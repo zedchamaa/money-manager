490e14c4cfc01b373158ded68b40b75274381825
Zed Chamaa <sales@nineprovince.com>
2023-03-15 12:46:59 +0100

    Update the income category items

--- src/components/CategoryMenuIncome.js
+++ src/components/CategoryMenuIncome.js
@@ -1,13 +1,25 @@
 import Select from 'react-select'
 
 export default function CategoryMenuIncome({ onChange, category }) {
-  const categories = [{ value: 'Salary', label: 'Salary' }]
+  const categories = [
+    { value: 'Child Support', label: 'Child Support' },
+    { value: 'Government Benefits', label: 'Government Benefits' },
+    { value: 'Investment Income', label: 'Investment Income' },
+    { value: 'Other Earning', label: 'Other Earning' },
+    { value: 'Pension Income', label: 'Pension Income' },
+    { value: 'Prize Winnings', label: 'Prize Winnings' },
+    { value: 'Rental Income', label: 'Rental Income' },
+    { value: 'Royalties', label: 'Royalties' },
+    { value: 'Salary', label: 'Salary' },
+    { value: 'Sale of Personal Property', label: 'Sale of Personal Property' },
+    { value: 'Other Income', label: 'Other Income' },
+  ]
 
   const menuContainer = document.body // or any other container with a max-width set
 
   return (
     <Select
-      options={categories}
+      options={categories.sort((a, b) => a.label.localeCompare(b.label))}
       onChange={onChange}
       placeholder={category}
       menuPortalTarget={menuContainer}
