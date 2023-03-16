56e59431039d187f3a260b68db9bffa4b057966a
Zed Chamaa <sales@nineprovince.com>
2023-03-16 12:56:23 +0100

    Create the mobile divider icon component

---
+++ src/components/icons/DividerMobile.js
@@ -0,0 +1,17 @@
+export default function DividerMobile() {
+  return (
+    <svg
+      width='342'
+      height='1'
+      viewBox='0 0 342 1'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        width='342'
+        height='1'
+        fill='#EAECF0'
+      />
+    </svg>
+  )
+}
