d0800f4f4271721c73509794ba54fd37cc2de8a8
Zed Chamaa <sales@nineprovince.com>
2023-03-17 09:39:47 +0100

    Create the divider desktop icon

---
+++ src/components/icons/DividerDesktop.js
@@ -0,0 +1,17 @@
+export default function DividerDesktop() {
+  return (
+    <svg
+      width='936'
+      height='1'
+      viewBox='0 0 936 1'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        width='936'
+        height='1'
+        fill='#EAECF0'
+      />
+    </svg>
+  )
+}
