69e23e8d0ca6dd8cc077ba567cf6d29643e21c1e
Zed Chamaa <sales@nineprovince.com>
2023-03-19 15:29:43 +0100

    Create the transactions history mobile component

---
+++ src/components/TransactionsHistoryMobile.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+// styles
+import styles from './TransactionsHistoryMobile.module.css'
+
+// components
+import SearchForm from './SearchForm'
+import TransactionsListMobile from './TransactionsListMobile'
+
+export default function TransactionsHistoryMobile({ transactionsByYear }) {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleChangeSearchTerm = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Transactions History</div>
+      <div className={styles.search}>
+        <SearchForm
+          onChange={handleChangeSearchTerm}
+          searchTerm={searchTerm}
+        />
+      </div>
+      <div className={styles.transactionsList}>
+        <TransactionsListMobile transactionsByYear={transactionsByYear} />
+      </div>
+      <div className={styles.pagination}>Pagination Menu</div>
+    </div>
+  )
+}
