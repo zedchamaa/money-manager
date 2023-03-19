74faa10c63f8e779f0fb2542d2c9faef741ff773
Zed Chamaa <sales@nineprovince.com>
2023-03-19 16:25:32 +0100

    Show loading message before fetching transactions

--- src/components/TransactionsListMobile.js
+++ src/components/TransactionsListMobile.js
@@ -1,12 +1,26 @@
+import { useState, useEffect } from 'react'
+
 // styles
 import styles from './TransactionsListMobile.module.css'
 
 export default function TransactionsListMobile({ transactionsByYear }) {
-  console.log(transactionsByYear)
+  const [alert, setAlert] = useState(false)
+
+  useEffect(() => {
+    if (transactionsByYear?.length === 0) {
+      setAlert(true)
+    }
+  }, [])
 
   const transactions = transactionsByYear?.map((transaction) => (
     <div key={transaction.id}>{transaction.amount}</div>
   ))
 
-  return transactions
+  return (
+    <>
+      {alert && <p>There are no transactions currently</p>}
+      {!transactions && <p>Loading...</p>}
+      {transactions}
+    </>
+  )
 }
