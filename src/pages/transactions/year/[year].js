e9e0410fbb745b55baebb78becf84496a33e1284
Zed Chamaa <sales@nineprovince.com>
2023-03-24 08:13:00 +0100

    Remove redundant use effect hook

--- src/pages/transactions/year/[year].js
+++ src/pages/transactions/year/[year].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import { useAuthContext } from '@/hooks/useAuthContext'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useCollection } from '@/hooks/useCollection'
 
 // components
