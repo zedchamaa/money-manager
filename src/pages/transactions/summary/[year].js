845659dcf3c0cbfcea568c1f32d7403a979798ff
Zed Chamaa <sales@nineprovince.com>
2023-03-24 12:18:01 +0100

    Display pie chart component on the summary page

--- src/pages/transactions/summary/[year].js
+++ src/pages/transactions/summary/[year].js
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router'
 import { useAuthContext } from '@/hooks/useAuthContext'
 import { useState } from 'react'
 
+// styles
+import styles from './summary.module.css'
+
 // components
 import MobileMenu from '@/components/MobileMenu'
 import DesktopMenu from '@/components/DesktopMenu'
@@ -10,12 +13,24 @@ import Modal from '@/components/Modal'
 import TransactionsForm from '@/components/TransactionsForm'
 import YearsCarouselMobile from '@/components/YearsCarouselMobile'
 import YearsCarouselDesktop from '@/components/YearsCarouselDesktop'
+import PieChart from '@/components/PieChart'
 
 export default function TransactionsSummary() {
   const [showModal, setShowModal] = useState(false)
   const router = useRouter()
   const { year } = router.query
   const { user } = useAuthContext()
+
+  // pie chart data for current year
+  const [currentYearData] = useState({
+    datasets: [
+      {
+        data: [1000, 5000],
+        backgroundColor: ['#7F56D9', '#E9D7FE'],
+        hoverOffset: 4,
+      },
+    ],
+  })
 
   // hide the page content from non-logged in users
   // always run this if statement first
@@ -48,6 +63,20 @@ export default function TransactionsSummary() {
       />
       <YearsCarouselMobile />
       <YearsCarouselDesktop />
+      <div className={styles.pieCharts}>
+        <PieChart
+          title='Year 2023'
+          chartData={currentYearData}
+          labelOne='Income'
+          labelTwo='Expenses'
+        />
+        <PieChart
+          title='Monthly Average'
+          chartData={currentYearData}
+          labelOne='Income'
+          labelTwo='Expenses'
+        />
+      </div>
     </>
   )
 }
