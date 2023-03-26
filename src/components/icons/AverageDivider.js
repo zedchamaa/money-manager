247e748dd6079e9248d28e31692828e7d31d9a75
Zed Chamaa <sales@nineprovince.com>
2023-03-26 12:07:58 +0200

    Create the average divider icon component

---
+++ src/components/icons/AverageDivider.js
@@ -0,0 +1,17 @@
+export default function AverageDivider() {
+  return (
+    <svg
+      width='176'
+      height='1'
+      viewBox='0 0 176 1'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        width='176'
+        height='1'
+        fill='#EAECF0'
+      />
+    </svg>
+  )
+}
