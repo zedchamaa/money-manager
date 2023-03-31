327648a0f8421494eb11826f8a5df69b73899796
Zed Chamaa <sales@nineprovince.com>
2023-03-31 15:45:35 +0200

    Undo hide firebase api keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDIX22ifcGB3ImTdY12kVgdDm69KUmVQGg',
+  authDomain: 'money-manager-demo-79ec4.firebaseapp.com',
+  projectId: 'money-manager-demo-79ec4',
+  storageBucket: 'money-manager-demo-79ec4.appspot.com',
+  messagingSenderId: '666624481732',
+  appId: '1:666624481732:web:2036b155c65d5e65d9ac91',
 }
 
 // init firebase
