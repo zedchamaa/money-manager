e2adad8ae2d3a7685aa944076491a7b22f024170
Zed Chamaa <sales@nineprovince.com>
2023-04-07 15:24:14 +0200

    Remove firebase app check

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,17 +16,6 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
 }
 
-const appCheck = firebase.appCheck()
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
-appCheck.activate(
-  '6LfWd2klAAAAAHxHxB3QaPCzudR1ynUuwjwdVGPk',
-
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  true
-)
-
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
